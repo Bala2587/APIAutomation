2f9615f60463f7a6861f54a3578fd1826479d306
Balakumar <balakumar.2587@gmail.com>
2022-10-12 22:54:59 -0400

    Add project files.

---
+++ APIAutomation/Testcase/GeocodingAPITestcases.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace APIAutomation.Testcase
+{
+    public class GeocodingAPITestcases
+    {
+        [Fact]
+        public void GetPostalcode()
+        {
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("us/20852", Method.Get);
+
+            RestResponse response = client.Execute(request);
+            var LocResponse = JsonConvert.DeserializeObject<locations>(response.Content);
+
+            Assert.Matches(LocResponse.PostCode, "20852");
+        }
+
+        [Fact]
+        public void GetPlacedetails()
+        {
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("us/20852", Method.Get);
+
+            RestResponse response = client.Execute(request);
+            var LocResponse = JsonConvert.DeserializeObject<locations>(response.Content);
+
+            Assert.Matches(LocResponse.Place[0].State, "washington");
+            Assert.Matches(LocResponse.Place[0].StateAbbreviation, "MD");
+
+        }
+
+
+        [Fact]
+        public void GetAge()
+        {
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("us/20852", Method.Get);
+
+            RestResponse response = client.Execute(request);
+            var LocResponse = JsonConvert.DeserializeObject<locations>(response.Content);
+
+            Assert.Matches(LocResponse.Place[0].State, "washington");
+            Assert.Matches(LocResponse.Place[0].StateAbbreviation, "MD");
+
+        }
+    }
+
+
+
+# region DTOClasses
+    class locations
+    {
+        [JsonProperty("post code")]
+        public string PostCode { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("country abbreviation")]
+        public string CountryAbbreviation { get; set; }
+        [JsonProperty("places")]
+        public List<places> Place { get; set; }
+
+
+    }
+
+    class places
+    {
+        [JsonProperty("place name")]
+        public string PlaceName { get; set; }
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+        [JsonProperty("state abbreviation")]
+        public string StateAbbreviation { get; set; }
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+    }
+    #endregion DTOClasses
+}
---
+++ APIAutomation/Testcase/PeopleActivityTestcases.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace APIAutomation.Testcase
+{
+    public class PeopleActivityTestcases
+    {
+        [Fact]
+        public void GetRandomActivity()
+        {
+            RestClient client = new RestClient("http://www.boredapi.com/api/activity/");
+            RestRequest request = new RestRequest();
+
+            RestResponse response = client.Execute(request);
+            var BoredResponse = JsonConvert.DeserializeObject<Bored>(response.Content);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public void GetActivityBasedonKey()
+        {
+            RestClient client = new RestClient("http://www.boredapi.com/api/activity");
+            RestRequest request = new RestRequest("?key=5881028",Method.Get);   
+            //request.AddParameter("key", "1017771", ParameterType.UrlSegment);
+
+            RestResponse response = client.Execute(request);
+            var BoredResponse = JsonConvert.DeserializeObject<Bored>(response.Content);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Matches(BoredResponse.type, "education");
+            Assert.Matches(BoredResponse.key, "5881028");
+        }
+    }
+
+
+
+    #region DTOclass
+    class Bored
+    {
+        [JsonProperty("activity")]
+        public string activity { get; set; }
+        [JsonProperty("type")]
+        public string type { get; set; }
+        [JsonProperty("participants")]
+        public string participants { get; set; }
+        [JsonProperty("key")]
+        public string key { get; set; }
+    }
+    #endregion DTOclass
+}
